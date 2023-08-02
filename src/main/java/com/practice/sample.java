e574a1b01621f52d7a83abedfdceca5da108a775
hg_c <aakkaa13579@gmail.com>
2023-08-02 12:21:46 +0900

    sample message 수정

--- src/main/java/com/practice/sample.java
+++ src/main/java/com/practice/sample.java
@@ -3,7 +3,7 @@ package com.practice;
 public class sample {
 
 	public static void main(String[] args) {
-		//본인 이름 작성 후 pull 해보기
+		//본인 이름 작성 후 push 해보기
 		System.out.println("최형규");
 		System.out.println("SAMPLE");
 		System.out.println("SAMPLE");
