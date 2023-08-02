13146a649965b4a2cc92105b10938cfb5c93715a
장한울 <jkea0419@naver.com>
2023-08-02 12:59:19 +0900

    텍스트 수정

--- src/main/java/com/practice/sample.java
+++ src/main/java/com/practice/sample.java
@@ -5,9 +5,9 @@ public class sample {
 	public static void main(String[] args) {
 		//본인 이름 작성 후 push 해보기
 		System.out.println("최형규");
-		System.out.println("김석호");
-		System.out.println("SAMPLE");
-		System.out.println("SAMPLE");
+		System.out.println("장한울");
+		System.out.println("ㅈㅎㅇ");
+		System.out.println("ㅋㅋㅋㅋ");
 		System.out.println("SAMPLE");
 		System.out.println("SAMPLE");
 		System.out.println("SAMPLE");
