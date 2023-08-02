9a168bd6d504a8e6e1e544a5f8acba014f9d7440
seokho <notdolseok@naver.com>
2023-08-02 12:47:21 +0900

    수정

--- src/main/java/com/practice/sample.java
+++ src/main/java/com/practice/sample.java
@@ -5,7 +5,7 @@ public class sample {
 	public static void main(String[] args) {
 		//본인 이름 작성 후 push 해보기
 		System.out.println("최형규");
-		System.out.println("SAMPLE");
+		System.out.println("김석호");
 		System.out.println("SAMPLE");
 		System.out.println("SAMPLE");
 		System.out.println("SAMPLE");
